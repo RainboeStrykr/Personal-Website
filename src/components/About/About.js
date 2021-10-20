1c69b1df0c09e1ee2b50b762e96a63ba4b0aff83
Abhiraj Bhowmick <87644009+RainboeStrykr@users.noreply.github.com>
2021-10-20 10:28:59 +0530

    Update About.js

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -11,20 +11,20 @@ const About = ({ name }) => {
 					<p>
 						I'm a web developer from India <i className="animated hover-bounce em-svg em-heart f-12" />
 						I'm also a content blogger. I create awesome stuff on the web. I am currently working on my startup 
-		                                <a href="https://twitter.com/visualwayorg">Visualway</a>.
+		                                <a href="https://twitter.com/visualwayorg"> Visualway</a>.
 					</p>
 					<p>
 						Some of my previous works:
 						<a rel="noopener noreferrer" target="_BLANK" href="https://markdownez.vercel.app">
-							MarkdownEz
+							 MarkdownEz
 						</a>
 						,
 						<a rel="noopener noreferrer" target="_BLANK" href="https://pixellate.netlify.app">
-							Pixellate
+							 Pixellate
 						</a>
 						,
 						<a rel="noopener noreferrer" target="_BLANK" href="https://webmockup.vercel.app/">
-							WebMockup
+							 WebMockup
 						</a>
 					</p>
 				</div>
